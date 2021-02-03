be99718ce516c5f47db9956efcefd28c15c6ad21
ohenriquemonte <montenegrohenrique@gmail.com>
2021-02-03 10:33:48 -0300

    Initial check-in of module ByteBank

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ContaCorrente conta = new ContaCorrente(123, 123123);
+
+			Console.WriteLine(conta.Numero);
+			Console.ReadLine();
+		}
+	}
+}
